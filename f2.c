a2b3b796e0cf9c3598feb9bdc095b5733399c672
Kiran <95173533+KiranBabu007@users.noreply.github.com>
2024-02-27 11:36:58 +0530

    Update f2.c

--- f2.c
+++ f2.c
@@ -8,3 +8,10 @@ printf("\nWe are in F2\n");
 printf("Second\nPID=%d\n",getpid());
 return 0;
 }
+
+
+// Terminal Code
+
+// csea2@sjcet-H81M-DS2:~/Kiran$ gcc f1.c -o f1
+// csea2@sjcet-H81M-DS2:~/Kiran$ gcc f2.c -o f2
+// csea2@sjcet-H81M-DS2:~/Kiran$ ./f1
