68379aabef1b4eac803855761c6d2ce6e110d746
kiranbabu3355 <kiranbabu3355@gmail.com>
2024-07-01 16:00:10 +0000

    leaky bucket finished

--- leaky-bucket/leaky-bucket.c
+++ leaky-bucket/leaky-bucket.c
@@ -2,8 +2,8 @@
 
 void main(){
     int n,incoming,outgoing,buck_size=50,store=0;
- printf("Enter the no. of incoming packets:");
-        scanf("%d",&n);
+ printf("Enter the no. of inputs and outgoing rate:");
+        scanf("%d %d",&n,&outgoing);
 
     while(n>0){
         printf("Enter the size of incoming packet:");
@@ -12,13 +12,18 @@ void main(){
         if(incoming<=(buck_size-store)){
             printf("Incoming packet size:%d\n",incoming);
             store+=incoming;
+            printf("bucket is filled %d out of %d\n",store,buck_size);
         }
         else{
             printf("Dropped packet size:%d\n",incoming-(buck_size-store));
             store=buck_size;
+            printf("bucket is filled %d out of %d\n",store,buck_size);
         }
+        store-=outgoing;
+        if (store<0){
+            store=0;
+        }
+        printf("bucket state after outgoing.. filled %d out of %d\n",store,buck_size);
         n--;
-        printf("bucket size after outgoing packet:%d\n",store-outgoing);
-
     }
 }
