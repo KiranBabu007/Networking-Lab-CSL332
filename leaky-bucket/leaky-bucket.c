fdd46b04f5642a570a7406a39a6282a603304ad1
kiranbabu3355 <kiranbabu3355@gmail.com>
2024-07-01 14:52:20 +0000

    initial code

--- leaky-bucket/leaky-bucket.c
+++ leaky-bucket/leaky-bucket.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 
 void main(){
-    printf("Hello");
+    int n,incoming,outgoing,buck_size=50,store=0;
+ printf("Enter the no. of incoming packets:");
+        scanf("%d",&n);
+
+    while(n>0){
+        printf("Enter the size of incoming packet:");
+        scanf("%d",&incoming);
+
+        if(incoming<=(buck_size-store)){
+            printf("Incoming packet size:%d\n",incoming);
+            store+=incoming;
+        }
+        else{
+            printf("Dropped packet size:%d\n",incoming-(buck_size-store));
+            store=buck_size;
+        }
+        n--;
+        printf("bucket size after outgoing packet:%d\n",store-outgoing);
+
+    }
 }
